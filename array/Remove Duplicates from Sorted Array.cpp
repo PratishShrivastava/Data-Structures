95bdb8ef7485d48718d1d2ef185230f4bde0bf6b
Pratish Shrivastava <65657554+PratishShrivastava@users.noreply.github.com>
2022-03-30 13:29:00 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- array/Remove Duplicates from Sorted Array.cpp
+++ array/Remove Duplicates from Sorted Array.cpp
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array
 
+// Do you notice something! if we do it normally we have to do it in O(n^2)
+// but after two pointer it is done in O(n).
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
