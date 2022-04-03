075351b3a33d1655e6aca6b12263b41f2164072c
Pratish Shrivastava <65657554+PratishShrivastava@users.noreply.github.com>
2022-04-03 23:24:48 +0530

    Create Find Pivot Index.cpp

---
+++ array/Find Pivot Index.cpp
@@ -0,0 +1,28 @@
+//  https://leetcode.com/problems/find-pivot-index/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int result = -1;
+        
+        int left[n],right[n];
+        left[0]=nums[0];
+        right[n-1]=nums[n-1];
+        
+        for(int i=1;i<n;++i){
+            left[i] = left[i-1]+nums[i];
+        }
+        for(int i=n-2;i>=0;--i){
+            right[i] = right[i+1]+nums[i];
+        }
+        for(int i=0;i<n;++i){
+            if(left[i]==right[i]){
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+};
+
