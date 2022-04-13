37c0d923332f731d7552e54148d41765f367c164
Pratish Shrivastava <65657554+PratishShrivastava@users.noreply.github.com>
2022-04-13 10:38:42 +0530

    Create Selection Sort.cpp

---
+++ array/Selection Sort.cpp
@@ -0,0 +1,33 @@
+// Selection Sort
+
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+    
+    return;
+}
+
+int main(){
+    int arr[] = {78,98,6,52,4,8,1,25,5,63,91,18,28};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=0;i<n;++i){
+        int j=0;
+        while(j<n){
+            if(arr[i]<arr[j]){
+                swap(arr[i], arr[j]);
+            }
+            j++;
+        }
+    }
+    
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
